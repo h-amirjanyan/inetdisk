cebcbc1a6ab51848448a0c5e358c1d5de3d6e29a
wanzhi09@gmail.com <wanzhi09@gmail.com@02ce4af4-647c-c39b-5aa0-fca55e3b7279>
2012-04-16 12:44:54 +0000

    添加项目代码

---
+++ trunk/NetDisk/NetDiskServer/Controllers/OpenController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetDiskServer.Controllers
+{
+    /// <summary>
+    /// Oauth验证接口
+    /// </summary>
+    public class OpenController : Controller
+    {
+        //
+        // GET: /Open/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// Requests the token.
+        /// </summary>
+        /// <returns> </returns>
+        public JsonResult RequestToken()
+        {
+            return Json("", JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// Authorizes this instance.
+        /// This Action will redirect to a webpage 
+        /// 在这个页面中会验证用户的口令，如果成功会跳转到call back。
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ViewResult Authorize()
+        {
+            //return Json("", JsonRequestBehavior.AllowGet);
+            //TODO:生成
+            return View();
+        }
+        
+        [HttpPost]
+        public ViewResult Authorize2()
+        {
+            //验证数据，并跳转到AuthorizeCompleted
+            return View();
+        }
+
+        public ViewResult AuthorizeCompleted()
+        {
+            int verifyCode = 0;//校验码
+            return View(verifyCode);
+        }
+
+        /// <summary>
+        /// Accesses the token.
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult AccessToken()
+        {
+            return Json("", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
