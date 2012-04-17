abb37d93a043a459d8110233cef73d3c891f0d46
wanzhi09@gmail.com <wanzhi09@gmail.com@02ce4af4-647c-c39b-5aa0-fca55e3b7279>
2012-04-17 02:19:51 +0000

    添加用户实体

---
+++ NetDisk/NetDiskServer/Models/NetDiskUserModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetDiskServer.Models
+{
+    /// <summary>
+    /// User Model
+    /// </summary>
+    public class NetDiskUser
+    {
+        /// <summary>
+        /// Gets or sets the user id.
+        /// </summary>
+        /// <value>
+        /// The user id.
+        /// </value>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the user.
+        /// </summary>
+        /// <value>
+        /// The name of the user.
+        /// </value>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the register date.
+        /// </summary>
+        /// <value>
+        /// The register date.
+        /// </value>
+        public DateTime RegisterDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last login date.
+        /// </summary>
+        /// <value>
+        /// The last login date.
+        /// </value>
+        public DateTime LastLoginDate { get; set; }
+    }
+}
