6c40ff09ab2e0c4ab91d6b6c72f2375164b47c5d
wanzhi09@gmail.com <wanzhi09@gmail.com@02ce4af4-647c-c39b-5aa0-fca55e3b7279>
2012-04-17 02:19:29 +0000

    添加oauth相关ViewModel

---
+++ trunk/NetDisk/NetDiskServer/ViewModels/OpenViewModels.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetDiskServer.ViewModels
+{
+    public class RequestTokenViewModel
+    {
+        /// <summary>
+        /// Gets or sets the oauth_token_secret.
+        /// </summary>
+        /// <value>
+        /// The oauth_token_secret.
+        /// </value>
+        public string oauth_token_secret { get; set; }
+
+        /// <summary>
+        /// Gets or sets the oauth_token.
+        /// </summary>
+        /// <value>
+        /// The oauth_token.
+        /// </value>
+        public string oauth_token { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="RequestTokenViewModel"/> is oauth_callback_confirmed.
+        /// </summary>
+        /// <value>
+        /// 	<c>true</c> if oauth_callback_confirmed; otherwise, <c>false</c>.
+        /// </value>
+        public bool oauth_callback_confirmed { get; set; }
+    }
+
+
+    public class Authorize2ViewModel
+    {
+        public string oauth_verifier { get; set; }
+    }
+
+    public class AccessTokenViewModel
+    {
+        public string oauth_token_secret { get; set; }
+
+        public string oauth_token { get; set; }
+
+        public int UserId { get; set; }
+
+        public string charged_dir { get; set; }
+    }
+}
