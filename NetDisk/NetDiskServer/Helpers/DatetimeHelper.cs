cfb43559a10d9f702304f049af4aa3071c519676
wanzhi09@gmail.com <wanzhi09@gmail.com@02ce4af4-647c-c39b-5aa0-fca55e3b7279>
2012-04-17 13:29:19 +0000

    添加时间转换方法

---
+++ NetDisk/NetDiskServer/Helpers/DatetimeHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetDiskServer.Helpers
+{
+    public static class Extension
+    {
+        public static double ConvertToUnixTimestamp(this DateTime date)
+        {
+            DateTime utc = date.ToUniversalTime();
+            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            TimeSpan diff = utc - origin;
+            return Math.Floor(diff.TotalSeconds);
+        }
+        public static DateTime ConvertFromUnixTimestamp(this double timestamp)
+        {
+            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return origin.AddSeconds(timestamp).ToLocalTime();
+        }
+    }
+
+ 
+    
+    //ConvertToUnixTimestamp(DateTime.UtcNow)
+
+//    static DateTime ConvertFromUnixTimestamp(double timestamp)
+//{
+//    DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+//    return origin.AddSeconds(timestamp);
+//}
+
+
+//static double ConvertToUnixTimestamp(DateTime date)
+//{
+//    DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+//    TimeSpan diff = date - origin;
+//    return Math.Floor(diff.TotalSeconds);
+//}
+}
